f99364393ba8f7cdd3395074c561c6e80b9ffcda
mackenzie <mackenzie@yourmoms>
2021-11-21 17:18:03 -0600

    Implement GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,7 +13,41 @@
 KillswitchAudioProcessorEditor::KillswitchAudioProcessorEditor (KillswitchAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    killswitch.setColour(juce::TextButton::buttonColourId, juce::Colours::grey);
+    setResizable(true,false);
+
+    auto sidebarMinHeight = (numOptions * (sideItemHeight + sideItemMargin)) + sideItemMargin;
+
+    setResizeLimits(sidebarMinWidth + killswitchMinSide + (sideItemMargin*3), juce::jmax(sidebarMinHeight, killswitchMinSide + (sideItemMargin * 2)), 10000, 10000);
+
+    sidebar.setColour(juce::Label::backgroundColourId, sidebarBGColour);
+    addAndMakeVisible(sidebar);
+
+    resumeNotesButton.setColour(juce::ToggleButton::tickColourId, textColour);
+    resumeNotesButton.setColour(juce::ToggleButton::tickDisabledColourId, textColour);
+    addAndMakeVisible(resumeNotesButton);
+    resumeNotesButton.addListener(this);
+
+    resumeNotesLabel.setText("Resume withheld MIDI notes", juce::dontSendNotification);
+    resumeNotesLabel.setColour(juce::Label::textColourId, textColour);
+    addAndMakeVisible(resumeNotesLabel);
+    resumeNotesConnector.reset(new juce::AudioProcessorValueTreeState::ButtonAttachment(audioProcessor.options, "RESUME_NOTES", resumeNotesButton));
+
+    blockNonNotesButton.setColour(juce::ToggleButton::tickColourId, textColour);
+    blockNonNotesButton.setColour(juce::ToggleButton::tickDisabledColourId, textColour);
+    addAndMakeVisible(blockNonNotesButton);
+    blockNonNotesButton.addListener(this);
+    blockNonNotesConnector.reset(new juce::AudioProcessorValueTreeState::ButtonAttachment(audioProcessor.options, "BLOCK_NON_NOTES", blockNonNotesButton));
+
+    midiOptionsLabel.setText("MIDI Options", juce::dontSendNotification);
+    midiOptionsLabel.setColour(juce::Label::textColourId, textColour);
+    midiOptionsLabel.setColour(juce::Label::backgroundColourId, optionsLabelBGColour);
+    addAndMakeVisible(midiOptionsLabel);
+
+    blockNonNotesLabel.setText("Block non-note messages", juce::dontSendNotification);
+    blockNonNotesLabel.setColour(juce::Label::textColourId, textColour);
+    addAndMakeVisible(blockNonNotesLabel);
+
+    killswitch.setColour(juce::TextButton::buttonColourId, buttonColour);
     killswitch.setButtonText("Kill");
     addAndMakeVisible (killswitch);
     killswitch.addListener(this);
@@ -31,12 +65,30 @@ KillswitchAudioProcessorEditor::~KillswitchAudioProcessorEditor()
 void KillswitchAudioProcessorEditor::paint (juce::Graphics& g)
 {   
     // fill the whole window white
-    g.fillAll (juce::Colours::white);
+    g.fillAll (bgColour);
 }
 
 void KillswitchAudioProcessorEditor::resized()
 {
-    killswitch.centreWithSize(100,100);
+    auto area = getLocalBounds();
+
+    auto sidebarArea = area.removeFromLeft(juce::jmax(sidebarMinWidth, area.getWidth() / 3));
+    sidebar.setBounds(sidebarArea);
+
+    auto subSidebarArea = sidebarArea.removeFromTop(sideItemHeight);
+    midiOptionsLabel.setBounds(subSidebarArea.removeFromTop(sideItemHeight));
+
+    subSidebarArea = sidebarArea.removeFromTop(sideItemHeight);
+    resumeNotesButton.setBounds(subSidebarArea.removeFromLeft(sideItemHeight).reduced(sideItemMargin));
+    resumeNotesLabel.setBounds(subSidebarArea);
+
+    subSidebarArea = sidebarArea.removeFromTop(sideItemHeight);
+    blockNonNotesButton.setBounds(subSidebarArea.removeFromLeft(sideItemHeight).reduced(sideItemMargin));
+    blockNonNotesLabel.setBounds(subSidebarArea);
+
+    auto killswitchWidth = juce::jmax(100, area.getWidth() / 4);
+    auto killswitchHeight = juce::jmax(100, area.getHeight() / 4);
+    killswitch.setBounds(area.getCentreX() - (killswitchWidth/2), area.getCentreY() - (killswitchHeight/2), killswitchWidth, killswitchHeight);
 }
 
 void KillswitchAudioProcessorEditor::buttonClicked (juce::Button* button)
@@ -45,8 +97,8 @@ void KillswitchAudioProcessorEditor::buttonClicked (juce::Button* button)
 
 void KillswitchAudioProcessorEditor::buttonStateChanged (juce::Button* button)
 {
-    if(button == &killswitch)
-        if(button->isDown())
+    if (button == &killswitch)
+        if (button->isDown())
             audioProcessor.killswitchOn->AudioParameterBool::operator=(true);
         else
             audioProcessor.killswitchOn->AudioParameterBool::operator=(false);
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,8 +28,34 @@ public:
     void resized() override;
 
 private:
+    // Buttons and labels
+    juce::Label sidebar;
     juce::TextButton killswitch;
-    
+    juce::ToggleButton resumeNotesButton;
+    juce::Label midiOptionsLabel;
+    juce::Label resumeNotesLabel;
+    juce::ToggleButton blockNonNotesButton;
+    juce::Label blockNonNotesLabel;
+
+    int numOptions = 3;
+
+    // Connectors
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> resumeNotesConnector;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> blockNonNotesConnector;
+
+    // Colors
+    juce::Colour bgColour = juce::Colour::fromString("#FF766174");
+    juce::Colour optionsLabelBGColour = juce::Colour::fromString("#FF281C26");
+    juce::Colour sidebarBGColour = juce::Colour::fromString("#FF543F52");
+    juce::Colour buttonColour = optionsLabelBGColour;
+    juce::Colour textColour = juce::Colour::fromString("#FFF0E8F3");
+
+    // Component Sizes
+    int sidebarMinWidth = 100;
+    int sideItemHeight = 40;
+    int sideItemMargin = 5;
+    int killswitchMinSide = 100;
+
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     KillswitchAudioProcessor& audioProcessor;
